71815c653f97ecd860fbab07a3e7877d99ab5672
Miguel-Costacurta <miguelfcostacurt@gmail.com>
2025-06-14 19:20:43 -0300

    melhorando a pesquisa e tambem o clique nos polos.

--- index.js
+++ index.js
@@ -1,37 +1,106 @@
 let position;
+const display = document.getElementById("divDisplay");
+const tabela = document.getElementById("tabela");
+let posPolo;
 
 function pesquisar(){
+    let encontrado;
     const pesquisa = document.getElementById("inputPesquisa");
     const termo = pesquisa.value.trim().toLowerCase();
 
     polos.forEach(polo =>{
         if(polo.nome.toLowerCase() === termo){
             encontrado = true;
+            posPolo = polo.pos;
         }
     });
 
     if(!encontrado){
-        console.log("Nao existe");
+        display.style.display = 'inline';
+        const linha = `
+            <tr>
+                <td>Digite um ponto válido</td>
+            </tr>`
+        tabela.innerHTML += linha;
     }
 
-    mostrarLista(encontrado);
+    pesquisaPolo();
 }
 
-function mostrarLista(polo){
-    const display = document.getElementById("divDisplay");
-    const tabela = document.getElementById("tabela");
-   const bike = polo.bikeQntd ?? 'N/A';
+function pesquisaPolo(){
+    display.style.display = 'inline';
+    polos.forEach(polo =>{
+        if(posPolo == polo.pos){
+            const bike = polo.bikeQntd;
+            const patinete = polo.patineteQntd;
+
+            if(bike > 0 && patinete > 0){
+                const linha = `
+                <tr>
+                    <td>Temos ${bike} bicicletas disponíveis!</td>
+                    <td>Temos ${patinete} patinetes disponíveis!</td>
+                </tr>`;
+                tabela.innerHTML = linha;
+            }else if(bike == 0 && patinete > 0){
+                const linha = `
+                <tr>
+                    <td>Infelizmente não temos bicicletas disponíveis!</td>
+                    <td>Temos ${patinete} patinetes disponíveis!</td>
+                </tr>`;
+                tabela.innerHTML = linha;
+            }else if(bike > 0 && patinete == 0){
+                const linha = `
+                <tr>
+                    <td>Temos ${bike} patinetes disponíveis!</td>
+                    <td>Infelizmente não temos patinetes disponíveis!</td>
+                </tr>`;
+                tabela.innerHTML = linha;
+            }else{
+                const linha = `
+                <tr>
+                    <td>Infelizmente não temos bicicletas disponíveis!</td>
+                    <td>Infelizmente não temos patinetes disponíveis!</td>
+                </tr>`;
+                tabela.innerHTML = linha;
+            }
+        }
+    })
+
+}
+
+function cliquePolo(polo){
+    const bike = polo.bikeQntd ?? 'N/A';
     const patinete = polo.patineteQntd ?? 'N/A';
 
     display.style.display = 'inline';
 
- const linha = `
+    if(bike > 0 && patinete > 0){
+        const linha = `
         <tr>
             <td>Temos ${bike} bicicletas disponíveis!</td>
             <td>Temos ${patinete} patinetes disponíveis!</td>
-        </tr>
-    `;
-
-    tabela.innerHTML = linha;
-
+        </tr>`;
+        tabela.innerHTML = linha;
+    }else if(bike == 0 && patinete > 0){
+        const linha = `
+        <tr>
+            <td>Infelizmente não temos bicicletas disponíveis!</td>
+            <td>Temos ${patinete} patinetes disponíveis!</td>
+        </tr>`;
+        tabela.innerHTML = linha;
+    }else if(bike > 0 && patinete == 0){
+        const linha = `
+        <tr>
+            <td>Temos ${bike} patinetes disponíveis!</td>
+            <td>Infelizmente não temos patinetes disponíveis!</td>
+        </tr>`;
+        tabela.innerHTML = linha;
+    }else{
+        const linha = `
+        <tr>
+            <td>Infelizmente não temos bicicletas disponíveis!</td>
+            <td>Infelizmente não temos patinetes disponíveis!</td>
+        </tr>`;
+        tabela.innerHTML = linha;
+    }
 }
--- mapa.js
+++ mapa.js
@@ -6,17 +6,17 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 const polos = [ 
     {pos: 1, nome: 'Hospital Universitario Regional', lat:-23.401262477520326, lng:-51.956334971464294, bikeQntd:5, patineteQntd:10},
-    {pos: 2, nome: 'Praça 7 De Setembro', lat:-23.422821164418053, lng:-51.953299297139914},
-    {pos: 3, nome: 'Estação Terminal Intermodal', lat:-23.418473, lng:-51.938098},
-    {pos: 4, nome: 'Vila Olimpica', lat:-23.41243379629831, lng:-51.9380194302094},
-    {pos: 5,nome: 'Praça Rocha Pombo', lat:-23.421404364983943, lng:-51.928969416709584},
-    {pos: 6, nome: 'Praça Farroupilha', lat:-23.401238275265484, lng:-51.92071718031649},
-    {pos: 7, nome: 'Parque do Inga', lat:-23.425683400781892, lng:-51.93287028787328},
-    {pos: 8, nome: 'Praça da Catedral', lat:-23.424571, lng:-51.938149},
-    {pos: 9, nome: 'Bosque II', lat:-23.43024470693862, lng:-51.94142637262559},
-    {pos: 10, nome: 'Praça Todos os Santos', lat:-23.43691020915539, lng:-51.93335911063106},
-    {pos: 11, nome: 'Praça Salgado Filho', lat:-23.43356860021524, lng:-51.90812317623513},
-    {pos: 12, nome: 'Estação Rodoviaria', lat:-23.425805433164545, lng:-51.910310576235204}
+    {pos: 2, nome: 'Praça 7 De Setembro', lat:-23.422821164418053, lng:-51.953299297139914, bikeQntd:3, patineteQntd:12},
+    {pos: 3, nome: 'Estação Terminal Intermodal', lat:-23.418473, lng:-51.938098, bikeQntd:10, patineteQntd:8},
+    {pos: 4, nome: 'Vila Olimpica', lat:-23.41243379629831, lng:-51.9380194302094, bikeQntd:9, patineteQntd:6},
+    {pos: 5,nome: 'Praça Rocha Pombo', lat:-23.421404364983943, lng:-51.928969416709584, bikeQntd:0, patineteQntd:2},
+    {pos: 6, nome: 'Praça Farroupilha', lat:-23.401238275265484, lng:-51.92071718031649, bikeQntd:4, patineteQntd:4},
+    {pos: 7, nome: 'Parque do Inga', lat:-23.425683400781892, lng:-51.93287028787328, bikeQntd:9, patineteQntd:8},
+    {pos: 8, nome: 'Praça da Catedral', lat:-23.424571, lng:-51.938149, bikeQntd:1, patineteQntd:11},
+    {pos: 9, nome: 'Bosque II', lat:-23.43024470693862, lng:-51.94142637262559, bikeQntd:11, patineteQntd:5},
+    {pos: 10, nome: 'Praça Todos os Santos', lat:-23.43691020915539, lng:-51.93335911063106, bikeQntd:2, patineteQntd:0},
+    {pos: 11, nome: 'Praça Salgado Filho', lat:-23.43356860021524, lng:-51.90812317623513, bikeQntd:0, patineteQntd:0},
+    {pos: 12, nome: 'Estação Rodoviaria', lat:-23.425805433164545, lng:-51.910310576235204, bikeQntd:8, patineteQntd:10}
 ];
 
 const marker = L.icon({
@@ -32,7 +32,7 @@ polos.forEach(polo => {
       .bindPopup(`<b>${polo.nome}</b>`);
 
   m.on('click', function () {
-      mostrarLista(polo); 
+      cliquePolo(polo); 
   });
 })
 
